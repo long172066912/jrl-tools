9d2d7818492f5e1d54d3334e1f2369ec127a460c
JerryLong <longjuncheng@moqipobing.com>
2024-10-30 18:18:17 +0800

    feat: lruCache测试

---
+++ tools/src/test/java/com/jrl/utils/JrlLruCacheTest.java
@@ -0,0 +1,17 @@
+package com.jrl.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JrlLruCacheTest {
+
+    @Test
+    void get() {
+        JrlLruCache<String, String> cache = new JrlLruCache<>(5);
+        for (int i = 0; i < 10; i++) {
+            cache.put(String.valueOf(i), String.valueOf(i));
+        }
+        assertEquals(5, cache.size());
+    }
+}
