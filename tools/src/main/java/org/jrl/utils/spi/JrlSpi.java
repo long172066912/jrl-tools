fc59d33085f796f41b3a5c95535631094025f6f3
JerryLong <172066912@qq.com>
2025-02-27 13:19:27 +0800

    fix: 定义spi注解，后续扩展spi加载逻辑

---
+++ tools/src/main/java/org/jrl/utils/spi/JrlSpi.java
@@ -0,0 +1,35 @@
+package org.jrl.utils.spi;
+
+import java.lang.annotation.*;
+
+/**
+ * spi扩展注解
+ *
+ * @author JerryLong
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+@Documented
+public @interface JrlSpi {
+    /**
+     * 扩展点名称
+     *
+     * @return
+     */
+    String value() default "";
+
+    /**
+     * 扩展点排序，高的优先
+     *
+     * @return
+     */
+    int order() default 0;
+
+    /**
+     * 扩展点分组，实现类请参考分组进行实现
+     * 使用 {@link JrlSpiGroup} 标注
+     *
+     * @return
+     */
+    String[] group() default {};
+}
