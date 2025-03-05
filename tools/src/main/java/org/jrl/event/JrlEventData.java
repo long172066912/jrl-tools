6d03ad846e331f5ddc1c386044d245ee3b79ab12
JerryLong <172066912@qq.com>
2025-03-05 13:55:24 +0800

    fix: 一个简单的事件处理

---
+++ tools/src/main/java/org/jrl/event/JrlEventData.java
@@ -0,0 +1,38 @@
+package org.jrl.event;
+
+
+/**
+ * @author JerryLong
+ * 事件数据
+ */
+public class JrlEventData<T> {
+    /**
+     * 如果指定监听器名称，则只通知该监听器
+     */
+    private String listenerName;
+    /**
+     * 数据
+     */
+    private T data;
+
+    public JrlEventData(String listenerName, T data) {
+        this.listenerName = listenerName;
+        this.data = data;
+    }
+
+    public String getListenerName() {
+        return listenerName;
+    }
+
+    public void setListenerName(String listenerName) {
+        this.listenerName = listenerName;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ tools/src/main/java/org/jrl/event/JrlEventListener.java
@@ -0,0 +1,38 @@
+package org.jrl.event;
+
+/**
+ * zeus事件监听器
+ *
+ * @author JerryLong
+ */
+public interface JrlEventListener<T> {
+    /**
+     * 监听的事件名称
+     *
+     * @return
+     */
+    String eventName();
+
+    /**
+     * 注册监听器
+     */
+    default void register() {
+        JrlEventBus.register(this);
+    }
+
+    /**
+     * 监听器名称
+     *
+     * @return
+     */
+    default String listenerName() {
+        return this.getClass().getName();
+    }
+
+    /**
+     * 监听到事件
+     *
+     * @param eventData 事件数据
+     */
+    void onEvent(T eventData);
+}
