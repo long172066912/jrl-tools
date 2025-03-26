4cf509cc4f8f0ff7928d32fd14dd516f3f4baad6
JerryLong <172066912@qq.com>
2025-03-26 10:51:03 +0800

    fix: redis

---
+++ monitor/jrl-monitor-micrometer/src/main/java/org/jrl/monitor/client/JrlMonitor.java
@@ -0,0 +1,14 @@
+package org.jrl.monitor.client;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+* 监控客户端
+* @author JerryLong
+*/
+public class JrlMonitor {
+    public static ExecutorService getMonitorExecutorService(String threadPoolName, ThreadPoolExecutor poolExecutor) {
+        return null;
+    }
+}
