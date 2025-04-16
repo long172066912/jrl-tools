b1e12b934de7fbbf77ea64c64d800f5f4fa5d5f7
JerryLong <172066912@qq.com>
2025-04-16 10:14:14 +0800

    feat: monitor

---
+++ monitor/jrl-monitor-micrometer/src/main/java/org/jrl/monitor/constant/MonitorConstants.java
@@ -0,0 +1,14 @@
+package org.jrl.monitor.constant;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+* 一些常量定义
+* @author JerryLong
+*/
+public interface MonitorConstants {
+    /**
+     * 时间单位统一毫秒
+     */
+    public static final TimeUnit DEFAULT_TIME_UNIT = TimeUnit.MILLISECONDS;
+}
